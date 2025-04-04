7da66c5902b48e365025c6b1a96566c0e70acccb
Egor Miroevsky <57674715+k0rdun@users.noreply.github.com>
2025-04-04 23:46:38 +0300

    feat: files download during the crawl

--- pdf_loader/loader.py
+++ pdf_loader/loader.py
@@ -9,7 +9,8 @@ from pdf_downloader import download
 
 FILENAME = 'u.json'
 DEBUG = False
-THREADS = 8
+THREADS_EXTRACTOR = 6
+THREADS_DOWNLOADER = 2
 
 
 def read_websites(filename: str) -> list:
@@ -30,6 +31,7 @@ def thread_handle_url(
     urls_set: set,
     pdfs_set: set,
     urls: queue.Queue,
+    pdfs_queue: queue.Queue,
     web_page: str
 ) -> None:
     '''Функция обработки очереди ссылок для потока'''
@@ -46,55 +48,68 @@ def thread_handle_url(
         free[num] = False
 
         # Извлечение всех ссылок со страницы
-        #print(url)
         website_urls = get_all_website_links(url, web_page)
 
         # Добавление полученных ссылок во множества и в очередь
-        pdfs_set.update(website_urls['pdf'])
         for internal_url in website_urls['url']:
             if internal_url not in urls_set:
                 urls.put(internal_url)
         urls_set.update(website_urls['url'])
+        for pdf_url in website_urls['pdf']:
+            if pdf_url not in pdfs_set:
+                pdfs_queue.put(pdf_url)
+        pdfs_set.update(website_urls['pdf'])
 
 
-def thread_download(q: queue.Queue, dir: str) -> None:
-    while not q.empty():
-        url, idx = q.get()
-        download(url, dir, idx)
+pdf_counter = 0
+
+def thread_download(q: queue.Queue, dir: str, free: list) -> None:
+    '''Функция скачивания pdf-файлов по ссылкам из очереди'''
+    global pdf_counter
+
+    while not q.empty() or not all(free):
+        if q.empty():
+            sleep(1)
+            continue
+        url = q.get()
+        # Использование pdf_counter не гарантирует правильную нумерацию
+        download(url, dir, pdf_counter)
+        pdf_counter += 1
 
 
 def handle_url(name: str, web_page: str) -> None:
     '''Обход по всему университету и скачивание всех pdf в директорию с названием университета'''
 
+    # Создание множеств посещённых ссылок
     urls_set = {web_page}
     pdfs_set = set()
-    free_extractors = [False] * THREADS
+    # Занятость потоков
+    free_extractors = [False] * THREADS_EXTRACTOR
 
+    # Создание очередей для ссылок сайта и для ссылок на pdf
     urls = queue.Queue()
+    pdfs_queue = queue.Queue()
     urls.put(web_page)
 
+    # Создание и запуск потоков на обход сайта
     threads_extractors = [Thread(
         target=thread_handle_url,
-        args=(free_extractors, i, urls_set, pdfs_set, urls, web_page)
-    ) for i in range(THREADS)]
-
+        args=(free_extractors, i, urls_set, pdfs_set, urls, pdfs_queue, web_page)
+    ) for i in range(THREADS_EXTRACTOR)]
     for thread in threads_extractors:
         thread.start()
-    for thread in threads_extractors:
-        thread.join()
 
-    # Скачивание всех pdf-файлов по ссылкам
-    urls = queue.Queue()
-    for idx, url in enumerate(pdfs_set):
-        urls.put((url, idx))
-    
+    # Создание и запуск потоков на Скачивание всех pdf-файлов по ссылкам
     threads_downloaders = [Thread(
         target=thread_download,
-        args=(urls, name)
-    ) for _ in range(THREADS)]
-
+        args=(pdfs_queue, name, free_extractors)
+    ) for _ in range(THREADS_DOWNLOADER)]
     for thread in threads_downloaders:
         thread.start()
+        
+    # Ожидание завершения потоков
+    for thread in threads_extractors:
+        thread.join()
     for thread in threads_downloaders:
         thread.join()
     
