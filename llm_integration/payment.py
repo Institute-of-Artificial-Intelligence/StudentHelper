366dd549100f0d76d008e3bee133f3a4a6b1ce22
Egor Miroevsky <egor932005@mail.ru>
2025-05-05 23:44:09 +0300

    feat: add payments check to server

---
+++ llm_integration/payment.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+from hashlib import sha256
+from os import getenv
+
+load_dotenv()
+terminal_key = getenv('TERMINAL_KEY')
+
+
+def tocken_check(data: dict) -> bool:
+    '''Проверка корректности токена в уведомлении'''
+    # Соединение значений в одну строку
+    values_list = []
+    for key in filter(lambda x: x != 'Token', sorted(data.keys())):
+        value = data[key]
+        if isinstance(value, bool):
+            value = 'true' if value else 'false'
+        values_list.append(str(value))
+    concatenated_values = ''.join(values_list)
+    # Сравнение получившегося токена с переданным
+    return sha256(concatenated_values.encode('ascii')).hexdigest() == data.get('Token')
+
+
+def confirmed_check(data: dict) -> bool:
+    '''Основная проверка на успешность проведения операции'''
+    return (
+        data.get('TerminalKey') == terminal_key
+        and data.get('Success')
+        and data.get('ErrorCode') == '0'
+        and data.get('Status') == 'CONFIRMED'
+    )
+
+
+def check_payment(data: dict) -> dict:
+    '''
+    Проверка успешности проведения платежа в уведомлении от Т-Банка.\n
+    :param data: JSON-уведомление от банка в виде словаря.
+    :return: Словарь с результатом проверки платежа.
+    '''
+    return {
+        'tocken': tocken_check(data),
+        'confirmed': confirmed_check(data)
+    }
--- llm_integration/server.py
+++ llm_integration/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from llm_agent import llm_agent, create_chat_history
+from llm_integration.llm_agent import llm_agent, create_chat_history
+from llm_integration.payment import check_payment
 from utils.database import chat_manager
 
 app = Flask(__name__)
@@ -24,6 +25,29 @@ def send_message():
     }
 
     return jsonify(response_data)
+
+
+@app.route('/api/payments/', methods=['POST'])
+def notigication_payment():
+    data = request.json
+
+    result = check_payment(data)
+    print(data, result, sep='\n')
+    amount = data.get('Amount')
+    if result['tocken'] and result['confirmed']:
+        print(f'Прошла успешно оплата заказа номер {data.get('OrderId')} на сумму {amount // 100}.{amount % 100}')
+    elif result['confirmed']:
+        print('Некорректная подпись уведомления')
+    elif result['tocken']:
+        print(
+            f'----- Статус заказа не является подтверждением платежа -----',
+            f'Номер заказа: {data.get('OrderId')}',
+            f'Статус заказа: {data.get('Status')}',
+            f'Успешность прохождения запроса: {data.get('Success')}',
+            f'Код и описание ошибки: {data.get('ErrorCode')} - {data.get('Details')}',
+            f'Сумма заказа: {amount // 100}.{amount % 100}',
+            sep='\n')
+    return 'OK'
 
 
 if __name__ == "__main__":
